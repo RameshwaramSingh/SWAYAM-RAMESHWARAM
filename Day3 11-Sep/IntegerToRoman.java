091cb91f9d2551bfd1aa04ddc9934a0d1e32d8c9
Rameshwaram Singh <rameshwaramsingh3@gmail.com>
2025-09-11 12:13:42 +0530

    Create IntegerToRoman.java

---
+++ Day3 11-Sep/IntegerToRoman.java
@@ -0,0 +1,19 @@
+//leetcode ques no 12
+https://leetcode.com/problems/integer-to-roman/description/
+
+class Solution {
+    public String intToRoman(int num) {
+        int[] vals =    {1000, 900, 500, 400, 100, 90,  50,  40,  10,  9,   5,  4,  1};
+        String[] syms = {"M",  "CM","D", "CD", "C", "XC","L", "XL", "X", "IX","V", "IV","I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0;i<vals.length;i++){
+            while(num>=vals[i]){
+                sb.append(syms[i]);
+                num = num-vals[i];
+            }
+        }
+        return sb.toString();
+    }
+}
